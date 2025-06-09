48c4e39d0933ead91a30f82f19ae6ec0c73206a8
jjoaa <jjoaa7@gmail.com>
2025-06-09 14:26:52 +0900

    Initial commit

---
+++ Models/ExchangeResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currency.Models
+{
+    public class ExchangeResponse
+    {
+        public double Amount { get; set; }
+        public string Base { get; set; }
+        public string Date { get; set; }
+        public Dictionary<string, double> Rates { get; set; }
+    }
+}
